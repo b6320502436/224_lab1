21c9288d35ef36ae14a86f58df5311b14fadbb37
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-04 18:56:59 +0800

    Update 6320502436_1_7.cpp

--- 6320502436_1_7.cpp
+++ 6320502436_1_7.cpp
@@ -5,11 +5,10 @@ int main()
     float a,b;
     int c,d;
 	scanf("%f",&a);
-	if(a>12)
+	if(a>12.00)
 	{
 		printf("%.0f:%d a.m.",a,(int)(a*100)%100);
-		}
-		else printf("%.0f:%.2d p.m.",a,(int)(a*100)%100);
+	}else printf("%.0f:%d p.m.",a,(int)(a*100)%100);
 
 	
 	
