bc34fff13d456cf691d4592a178607380c0834a1
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-04 18:33:58 +0800

    Create 6320502436_1_6.cpp

---
+++ 6320502436_1_6.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int n,m,d;
+	unsigned long long int t;
+	char a;
+	scanf("%d",&n);
+	scanf("%d",&m);
+	scanf("%llu",&t);
+	fflush(stdin);
+	scanf("%c",&a);
+	if(t>n&&t>m){
+
+	if(a=='L')
+	{
+		if((t/n)%2)
+		{
+			printf("R");
+		}
+		else printf("L");
+	}
+	if(a=='R')
+	{
+		if((t/m)%2)
+		{
+			printf("L");
+		}
+		else printf("R");
+	}	}
+	return 0;
+	
+	
+}
