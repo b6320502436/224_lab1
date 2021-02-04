4ea060eb672951e4238d2c5bfefcd6a750abf4cd
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-04 18:01:11 +0800

    Update 6320502436_1_5.cpp

--- 6320502436_1_5.cpp
+++ 6320502436_1_5.cpp
@@ -1,20 +1,21 @@
 #include<stdio.h>
 int main()
 {
-	int a,b=0,c,i;
+	int a,b=0,c=0,i,d=0;
 	scanf("%d",&a);
-	printf("%d",a);
-	int N[a]={};
+	int N[a];
+	
 	for(i=0;i<a;i++)
-	{scanf("%d",&N[i]);
-	if(N[i]>a)
 	{
-		a=N[i];
+	scanf("%d",&N[i]);
+	if(N[i]>b)
+	{
+		b=N[i];
 		c=i;
 	}
 
 }
-	printf("%d %d",c,a);
+	printf("%d %d",c+1,b);
 
 	return 0;
 	
